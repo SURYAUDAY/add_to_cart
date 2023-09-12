dab33fff7ddc54eff098c1ced9bafb47dc7f3198
suryauday prakash mishra <64128164+SURYAUDAY@users.noreply.github.com>
2023-09-12 10:06:43 +0530

    extra features added

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
     const leftContainer = document.getElementById("left-container");
     const rightContainer = document.getElementById("right-container");
+    const addedItems = new Set();
 
-    
-    function addRowToCart(rowData) {
-        const row = rowData.cloneNode(true);
-        const addButton = row.querySelector(".heart-button");
-        const actionCell = row.querySelector("td:last-child");
+    function rowInCart(rowData) {
+        const itemId = rowData.dataset.id;
 
-        
-        addButton.parentNode.innerHTML = "";
-        
-        row.removeChild(actionCell);
+        if (addedItems.has(itemId)) {
+            const rowInCart = rightContainer.querySelector(`[data-id="${itemId}"]`);
+            rowInCart.remove();
+            addedItems.delete(itemId);
+            const leftButton = rowData.querySelector(".heart-button");
+            leftButton.style.color = "red";
+        } else {
+           
+            const row = rowData.cloneNode(true);
+            const addButton = row.querySelector(".heart-button");
 
-        
-        rightContainer.querySelector("tbody").appendChild(row);
+           
+            addButton.parentNode.innerHTML = "";
 
-        
-        row.classList.add("added");
+            
+            rightContainer.querySelector("tbody").appendChild(row);
+
+            
+            row.classList.add("added");
+
+            
+            addedItems.add(itemId);
+
+           
+            const leftButton = rowData.querySelector(".heart-button");
+            leftButton.style.color = "grey";
+        }
     }
 
     
@@ -29,18 +44,20 @@ document.addEventListener("DOMContentLoaded", function () {
             
             data.forEach(values => {
                 const row = document.createElement("tr");
+                row.dataset.id = values.id;
                 row.innerHTML = `
                 <td>${values.title}</td>
                 <td>${values.description}</td>
                 <td>${values.price}</td>
                 <td><img src="${values.image}"/></td>
-                <td><button class="heart-button">❤️</button></td>
+               <td><i class="fa-solid fa-heart heart-button" style="color: #f70202;"></i></td>
                 `;
 
                 
                 const heartButton = row.querySelector(".heart-button");
                 heartButton.addEventListener("click", () => {
-                    addRowToCart(row); 
+                    
+                    rowInCart(row); 
                 });
 
                 tbody.appendChild(row);
